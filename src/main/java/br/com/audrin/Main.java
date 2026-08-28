702c750613734b3658898ad523c540455a591c45
a-henrique <audrinh@gmail.com>
2026-08-28 00:06:20 -0300

    clean mainn()

--- src/main/java/br/com/audrin/Main.java
+++ src/main/java/br/com/audrin/Main.java
@@ -8,18 +8,5 @@ import br.com.audrin.library.service.UserService;
 public class Main {
     public static void main(String[] args) {
 
-        UserRepository userRepository = new InMemoryUserRepository();
-        UserService userService = new UserService(userRepository);
-
-        User user1 = new User(1L, "Audrin", "audrin@123.com");
-        User user2 = new User(2L, "Outro Audrin", "audrin@123.com");
-
-        userService.create(user1);
-        System.out.println("Usuario 1 cadastrado");
-        try {
-            userService.create(user2);
-        } catch (IllegalArgumentException exception){
-            System.out.println(exception.getMessage());
-        }
     }
 }
